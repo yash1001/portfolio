5182c0783c205347250a311c03821e5f2571b019
yash chaturvedi <yash.chat10@gmail.com>
2020-11-07 18:41:10 -0500

    index page... links remain

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,11 @@
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ]
 		});
 
 	// Play initial animations on page load.
